33021855a364ffb91a79b49dc3388a33d62ce383
Alan Thomas Ramirez <thomasramirez0806@gmail.com>
2025-05-12 19:02:18 -0500

    feat :sparkles: Adding new functions

--- Dia6/Ejercicio1_Dia6_RamirezAlan.py
+++ Dia6/Ejercicio1_Dia6_RamirezAlan.py
@@ -3,6 +3,11 @@
 # Diccionarios
 # Terminar el ejercicio
 
+import os
+
+def limpiar_pantalla():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
 ##Diccionario con listas
 diccionarioRobusto={
     "id":1,
@@ -23,12 +28,15 @@ diccionarioRobusto2={
 listaRobusta=[]
 listaRobusta.append(diccionarioRobusto)
 listaRobusta.append(diccionarioRobusto2)
-
+print("")
+print("Bienvenido a la Librería de personas")
+print("")
 booleanito = True
 while(booleanito):
-    print("#################")
+    print("-----------------------------")
     print("--- Librería de personas ---")
-    print("#################")
+    print("-----------------------------")
+    print("")
     #CRUD (CREATE , READ , UPDATE & DELETE)
     print("1. Crear Persona")
     print("2. Mostrar todas las personas")
@@ -36,11 +44,12 @@ while(booleanito):
     print("4. Actualizar a una persona en específico")
     print("5. Eliminar a una persona en específico")
     print("6. Cerrar programa")
-    opcionUsuario=int(input("Escoja una opción (Numérica):"))
+    opcionUsuario=int(input("Escoja una opción (Numérica): "))
+    print("")
     if(opcionUsuario==1):
-        print("#################")
+        print("-----------------------------")
         print("--- Crear Persona ---")
-        print("#################")
+        print("-----------------------------")
         diccionarioVacio={
     "id":int(input("Ingrese el ID de la nueva persona: ")),
     "nombre":str(input("Ingrese el Nombre de la nueva persona: ")),
@@ -58,28 +67,99 @@ while(booleanito):
 
     elif(opcionUsuario==2):
         for i in range(len(listaRobusta)):
-            print("#################")
-            print("#### Persona #",i+1," ####")
-            print("#################")
-            print("ID:", listaRobusta[i]["id"])
+            print("-----------------------------")
+            print("--- Persona #",i+1," ---")
+            print("-----------------------------")
+            print("")
+            print("Id:", listaRobusta[i]["id"])
             print("Nombre:",listaRobusta[i]["nombre"])
             print("Apellido:",listaRobusta[i]["apellido"])
             print("Edad",listaRobusta[i]["edad"])
             
             for q in range(len(listaRobusta[i]["telefonos"])):
-                print("---------------------------")
-                print("Telefono#",q+1,":")
+                print(f"Telefono #{q+1}:")
                 print("#### - Código:",listaRobusta[i]["telefonos"][q]["codigo"])
                 print("#### - Numero:",listaRobusta[i]["telefonos"][q]["numero"])
                 print("#### - Tipo:",listaRobusta[i]["telefonos"][q]["tipo"])
-                
-                print("---------------------------")
+            print("")
+    elif(opcionUsuario==3):
+        numBuscar = int(input("Ingresa el Id de la persona a mostrar: "))
+        persona = next((i for i in listaRobusta if i["id"] == numBuscar), None)
+        print("")
+        if persona:
+            print("-----------------------------")
+            print("--- Persona encontrada ---")
+            print("-----------------------------")
+            print("")
+            print("Id:", persona["id"])
+            print("Nombre:", persona["nombre"])
+            print("Apellido:", persona["apellido"])
+            print("Edad:", persona["edad"])
 
-            
-            
+            for q in range(len(persona["telefonos"])):
+                print(f"Teléfono #{q+1}:")
+                print("#### - Código:", persona["telefonos"][q]["codigo"])
+                print("#### - Número:", persona["telefonos"][q]["numero"])
+                print("#### - Tipo:", persona["telefonos"][q]["tipo"])
+                print("")
+        else:
+            print("Persona no encontrada.")
+            print("")
+    elif(opcionUsuario==4):
+        numBuscar = int(input("Ingresa el Id de la persona a editar: "))
+        persona = next((i for i in listaRobusta if i["id"] == numBuscar), None)
+        print("")
+        if persona:
+            print("-----------------------------")
+            print("--- Persona encontrada ---")
+            print("-----------------------------")
+            print("")
+            print("Acontinuación ingrese los nuevos valores de la persona:")
+            print("")
+            persona["id"]=int(input("Id: "))
+            persona["nombre"]=str(input("Nombre: "))
+            persona["apellido"]=str(input("Apellido: "))
+            persona["edad"]=int(input("Edad: "))
+
+            for q in range(len(persona["telefonos"])):
+                print(f"Teléfono #{q+1}:")
+                persona["telefonos"][q]["codigo"]=int(input("#### - Código: "))
+                persona["telefonos"][q]["numero"]=int(input("#### - Número: "))
+                persona["telefonos"][q]["tipo"]=str(input("#### - Tipo: "))
+                print("")
+            print("")
+            print("*******************************")
+            print("LOS DATOS HAN SIDO ACTUALIZADOS")
+            print("*******************************")
+            print("")
+        else:
+            print("Persona no encontrada.")
+            print("")
+                   
+    elif(opcionUsuario==5):
+        numBuscar = int(input("Ingresa el Id de la persona a eliminar: "))
+        persona = next((i for i in listaRobusta if i["id"] == numBuscar), None)
+        if persona:
+            print("Estas seguro que quieres ELIMINAR a Id:", persona["id"], persona["nombre"], persona["apellido"])
+            print("1. Sí")
+            print("2. No")
+            if int(input("Escoja una opción (Numérica): "))==1:
+                listaRobusta.remove(persona)
+                print("")
+                print("La Persona ha sido ELIMINADA")
+                print("")
+            else:
+                print("La Persona no se ha eliminado")
+                print("")
+        else:
+            print("")
+            print("Persona no encontrada.")
+            print("")
+
     elif(opcionUsuario==6):
         print("Chaousssss")
+        print("")
         booleanito=False
     else:
         print("No es una opción válida")
-
+        print("")
